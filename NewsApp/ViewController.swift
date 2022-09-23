e546ec6f662332a5ffc0c25b61fde4adf3c8735f
Aung Ko Ko <alieaungko@gmail.com>
2022-09-23 10:12:18 +0630

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Aung Ko Ko on 23/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
